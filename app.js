be85d2dd3221b86868f043bc59674d7002be693d
Kain Nhantumbo <nhantumbok@gmail.com>
2021-12-15 18:39:17 -0300

    Fim do desenvolvimento da aplicação base.

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
+window.addEventListener('load', function () {
+    const btnSubmit = document.querySelector('#btnSubmit');
+    const btnReseta = document.getElementById('reset-values');
+    const monitor = document.querySelector('.display-result');
 
-const monitor = document.querySelector('.display-result');
-const btnSubmit = document.querySelector('#btnSubmit');
-const nome = document.querySelector('#nome').value;
-// const idade = document.querySelector('#idade').value;
-const altura = document.querySelector('#altura').value;
-const peso = document.querySelector('#peso').value;
+    btnSubmit.addEventListener('click', function (e) {
+        e.preventDefault();
+        const nome = document.getElementById('nome').value;
+        const altura = document.getElementById('altura').value;
+        const peso = document.getElementById('peso').value;
+        
+        
 
-btnSubmit.addEventListener('click', function (e) {
-    e.preventDefault();
+        const valorIMC = (peso / Math.pow(altura,2)).toFixed(2);
+        const classificacao = classificar(valorIMC);
 
-    console.log(peso)
-    console.log(altura)
-    
-    const valorIMC = (peso / (altura * altura));
-    
-    console.log(`Olá ${nome}, o seu IMC é de ${valorIMC}.`);
+        if (valorIMC === NaN || classificacao === undefined)
+        return monitor.textContent = 'Campo vazio detectado. Preencha devidamente os campos.';
+        return monitor.textContent = `Olá ${nome}, o seu IMC é de ${valorIMC} e está ${classificacao}.`;
+    });
 
+    btnReseta.addEventListener('click', () => {
+        monitor.textContent = '';
+    });
 
-})
+    function classificar (valorIMC) {
+        let classificacao = [
+            'abaixo do peso',
+            'com peso ideal. Continue assim!',
+            'levente acima do peso',
+            'com obesidade de grau I',
+            'com obesidade de grau II',
+            'com obesidade grau III. Cuidado!'
+        ];
 
+        if (valorIMC < 18.5)
+        return classificacao[0];
 
+        if (valorIMC < 25)
+        return classificacao[1];
+
+        if (valorIMC < 30)
+        return classificacao[2];
+        
+        if (valorIMC < 35)
+        return classificacao[3];
+
+        if (valorIMC <= 40)
+        return classificacao[4];
+
+        if (valorIMC > 40)
+        return classificacao[5];
+    }
+});
