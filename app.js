e64362fb98a24a52de38ca802f54d003d21773ac
KainNhantumbo <nhantumbok@gmail.com>
2022-01-20 02:48:00 +0200

    Adicionado o arquivo de readme e melhorias na documentação do código. Melhorias na interface do aplicativo.

--- app.js
+++ app.js
@@ -5,34 +5,43 @@ window.addEventListener('load', function () {
 
     btnSubmit.addEventListener('click', function (e) {
         e.preventDefault();
+        // captura os dados inseridos no formulário e armazena
+        // em variáveis.
         const nome = document.getElementById('nome').value;
         const altura = document.getElementById('altura').value;
         const peso = document.getElementById('peso').value;
         
-        
-
+        // faz o cálculo de IMC
         const valorIMC = (peso / Math.pow(altura,2)).toFixed(2);
+
+        // classifica com base no resultado do cálculo de IMC
         const classificacao = classificar(valorIMC);
 
+        // verifica se os campos de valor e se classificação foi bem sucedida,
+        // caso contrário exibe mensagens de erro no monitor.
         if (valorIMC === NaN || classificacao === undefined)
         return monitor.textContent = 'Campo vazio detectado. Preencha devidamente os campos.';
-        return monitor.textContent = `Olá ${nome}, o seu IMC é de ${valorIMC} e está ${classificacao}.`;
+        return monitor.textContent = `Olá ${nome}, o seu IMC é de ${valorIMC} e está ${classificacao}`;
     });
 
+    // limpa os campos do formulário
     btnReseta.addEventListener('click', () => {
         monitor.textContent = '';
     });
 
+    // cria as mensagens para o monitor
     function classificar (valorIMC) {
-        let classificacao = [
-            'abaixo do peso',
+        // mensagens a serem exibidas no monitor
+        const classificacao = [
+            'abaixo do peso.',
             'com peso ideal. Continue assim!',
-            'levente acima do peso',
-            'com obesidade de grau I',
-            'com obesidade de grau II',
+            'levente acima do peso.',
+            'com obesidade de grau I.',
+            'com obesidade de grau II.',
             'com obesidade grau III. Cuidado!'
         ];
 
+        // condições em que cada mensagem será exibida no monitor.
         if (valorIMC < 18.5)
         return classificacao[0];
 
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Aplicativo de Cálculo de IMC
+Esta aplicação calcula o *Índice de Massa Corporal* (IMC) com base na leitura de dados introduzidos pelo usuário, como a altura e o peso atual.
+
+## Cálculo
+Para o cálculo, são usadas as seguintes propriedades: a altura e o peso. O nome pedido ao usuário serve apenas para montar a estrutura da mensagem de resposta após o cálculo.
+
+## Persistência de Dados
+Este aplicativo não guarda nenhum dados do usuário nem faz backups em nuvem.
+
+
+### Contato
+Para saber mais sobre esta aplicação, entre em contacto comigo através do e-mail:
+nhantumbok@gmail.com
